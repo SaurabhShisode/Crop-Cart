9f08ec5b72ee8e3b08a4f56a756009d8520234b7
SaurabhShisode <119099005+SaurabhShisode@users.noreply.github.com>
2025-06-07 14:59:23 +0530

    Update FarmerDashboard.tsx

--- client/src/pages/FarmerDashboard.tsx
+++ client/src/pages/FarmerDashboard.tsx
@@ -677,8 +677,9 @@ const FarmerDashboard: React.FC = () => {
                 <div className="col-span-full sm:col-span-1 flex items-end">
                   <button
                     type="submit"
-                    className="w-full bg-green-600 hover:bg-green-700 text-white py-3 rounded-lg font-semibold shadow-md transition duration-300 ease-in-out"
+                    className="w-full bg-gradient-to-br from-green-900 to-emerald-800 rounded-2xl text-white py-3 font-semibold shadow-md transition duration-300 ease-in-out hover:from-green-800 hover:to-emerald-700"
                   >
+
                     Add Crop
                   </button>
                 </div>
