2422e7ea3d1fcae9f718287bc741a66a9f16d737
SaurabhShisode <119099005+SaurabhShisode@users.noreply.github.com>
2025-06-08 15:38:37 +0530

    Update CheckoutPage.tsx

--- client/src/pages/CheckoutPage.tsx
+++ client/src/pages/CheckoutPage.tsx
@@ -112,6 +112,10 @@ const CheckoutPage: React.FC = () => {
   if (cart.length === 0) {
     return (
       <div className="min-h-screen flex flex-col items-center justify-center  px-6 text-center">
+        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-16">
+          <path strokeLinecap="round" strokeLinejoin="round" d="M2.25 3h1.386c.51 0 .955.343 1.087.835l.383 1.437M7.5 14.25a3 3 0 0 0-3 3h15.75m-12.75-3h11.218c1.121-2.3 2.1-4.684 2.924-7.138a60.114 60.114 0 0 0-16.536-1.84M7.5 14.25 5.106 5.272M6 20.25a.75.75 0 1 1-1.5 0 .75.75 0 0 1 1.5 0Zm12.75 0a.75.75 0 1 1-1.5 0 .75.75 0 0 1 1.5 0Z" />
+        </svg>
+
         <h2 className="text-3xl font-semibold text-gray-700 mb-4">Your cart is empty</h2>
         <p className="text-gray-500">Please add some items to your cart before checking out.</p>
       </div>
@@ -121,10 +125,10 @@ const CheckoutPage: React.FC = () => {
     let updatedCart;
 
     if (newQuantity <= 0) {
-      
+
       updatedCart = cart.filter(item => item._id !== itemId);
     } else {
-      
+
       updatedCart = cart.map(item =>
         item._id === itemId
           ? { ...item, quantityInCart: newQuantity }
